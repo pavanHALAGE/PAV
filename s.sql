6b918cea49fe7b74590f068f834de4135f37a027
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-04-30 21:31:29 +0530

    Book table is created

--- s.sql
+++ s.sql
@@ -6,3 +6,10 @@ ADDRESS varchar(50) NOT NULL,
 PHONE varchar(20));
 
 desc PUBLISHER;
+
+Create table BOOK(
+BOOK_ID INT primary Key,
+TITLE varchar(20) not null,
+Publisher_Name varchar(20) references PUBLISHER(Name) ON delete cascade,
+PUB_YEAR varchar(5));
+
